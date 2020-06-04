631cf724f4c3a53423a3f38a7bb427d9d7c14bda
atulkamble11 <raj.kambale@yahoo.com>
2020-06-04 11:21:42 +0530

    added comment as per suggestion

--- GitProjectV2/src/testpack/Test2.java
+++ GitProjectV2/src/testpack/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("in test 2");
+System.out.println("in test 2  ...adeed comment");
 	}
 
 }
